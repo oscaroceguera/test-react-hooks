a3dd8ee7b0ce0c0ebf57cd7fdc6260c9e346535d
oscar <oscaroceguera@gmail.com>
2020-01-12 20:28:38 -0700

    add react-with-redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useContext } from "react";
+import { Store } from "./store";
+import { createNewListitem, deleteNewListItem } from "./actions";
+import './app.scss';
 
 function App() {
+  const { state, dispatch } = useContext(Store);
+
+  const input = useRef();
+
+  const onFormSubmit = event => {
+    event.preventDefault();
+
+    const listItemValue = input.current.value
+
+    if (listItemValue !== "") {
+      createNewListitem({
+        dispatch,
+        payload: { listItemValue: input.current.value }
+      });
+
+      input.current.value = ""
+    }
+  };
+
+   const onDeleteListItem = uuid => {
+     deleteNewListItem({
+       dispatch,
+       payload: {
+         uuid
+       }
+     });
+   };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="app">
+      <h1 className="app__title">To do list</h1>
+      <form className="app__form" onSubmit={onFormSubmit}>
+        <input type="text" className="app__input" ref={input} />
+        <input
+          type="submit"
+          className="app__input-submit"
+          value="Add to list"
+        />
+      </form>
+      <ul className="app__list">
+        {state.list.map((listItem, index) => (
+          <li className="app__list-item" key={`list-item-${index}`}>
+            {listItem.listItemValue}
+            <button
+              className="app__list-item-delete"
+              onClick={() => onDeleteListItem(listItem.uuid)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
 
---
+++ src/actions.js
@@ -0,0 +1,12 @@
+export const createNewListitem = ({dispatch, payload}) => {
+  return dispatch({
+    type: "CREATE_NEW_LIST_ITEM",
+    payload
+  });
+}
+export const deleteNewListItem = ({dispatch, payload}) => {
+  return dispatch({
+    type: "DELETE_LIST_ITEM",
+    payload
+  });
+}
